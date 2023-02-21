c0d05ca184fb5b18f0a7817c2a9ab4a30f365259
triplee12 <tripleeoliver@gmail.com>
2023-02-21 01:22:07 +0100

    Simple crud api with postgresql

--- apps/main.py
+++ apps/main.py
@@ -12,7 +12,7 @@ while True:
     try:
         with psycopg2.connect(
                 host="127.0.0.1",
-                dbname="your_dbname",
+                dbname="dbname",
                 user="user",
                 password="password",
                 cursor_factory=RealDictCursor
@@ -21,19 +21,15 @@ while True:
             print("Connection established!")
             break
     except Exception as e:
-        print(f"Error occurred while connecting to database. {e}")
+        print(f"Error occurred while connecting to database. \n{e}")
         time.sleep(2000)
 
 
 class PostModel(BaseModel):
     """PostModel schema"""
-    id: str = uuid.uuid4()
     title: str
     content: str
-    created_at: str = datetime.datetime.now()
-    updated_at: str = datetime.datetime.now()
     published: bool = False
-    rating: Optional[int] = None
 
     def __str__(self) -> str:
         return self.title
@@ -81,39 +77,58 @@ async def root():
 @app.get("/posts")
 async def get_posts():
     """Retrives all posts"""
-    cursor.execute("SELECT * FROM posts")
+    cursor.execute("""SELECT * FROM posts""")
     posts = cursor.fetchall()
     return {"posts": posts}
 
 @app.get("/posts/{id}")
 def get_post(id: int):
     """Return post details"""
-    post = get_post_by_id(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id),))
+    post = cursor.fetchone()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found error")
     return {"post": post}
 
 @app.put("/posts/update/{id}")
-def update_post(id: int, new_data: PostModel):
+def update_post(id: int, post: PostModel):
     """Updates post details"""
-    indx = find_post_index(id)
-    new_data_dct = new_data.dict()
-    new_data_dct["id"] = id
-    my_posts[indx] = new_data_dct
-    post = get_post_by_id(id)
-    return {"post": post}
+    cursor.execute(
+        """
+        UPDATE posts SET title = %s, content = %s, published = %s,
+        updated_at = NOW() WHERE id = %s RETURNING *
+        """, (post.title, post.content, post.published, str(id),)
+    )
+    updated = cursor.fetchone()
+    if updated:
+        conn.commit()
+        return {"post": updated}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found error")
 
 @app.delete("/posts/delete/{id}")
 def delete_post(id: int, response: Response) -> Response:
     """Delete post"""
-    indx = find_post_index(id)
-    my_posts.pop(indx)
-    response.status_code = status.HTTP_204_NO_CONTENT
-    return response.status_code
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
+    deleted = cursor.fetchone()
+    if deleted:
+        conn.commit()
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return response.status_code
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found error")
 
 @app.post("/create/posts")
 async def create_post(post: PostModel, response: Response):
     """Create new post"""
+    cursor.execute(
+        """
+        INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) 
+        RETURNING *
+        """,
+        (post.title, post.content, post.published)
+    )
+    post = cursor.fetchone()
+    conn.commit()
     if post:
         response.status_code = status.HTTP_201_CREATED
-        my_posts.append(post.dict())
         return {"post": post}
     raise HTTPException(status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Post not created")
